cdc39cd86b315a2e7160de69ff2efaca62f5eb97
Samantha Lopez <150631576+SamanthaLProgramming@users.noreply.github.com>
2024-04-12 09:40:36 -0700

    Add files via upload

---
+++ Welcome!/js/main.js
@@ -0,0 +1,164 @@
+// Welcome!
+// Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+// The Task
+// Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+// Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+// The Database
+// [ ("english", "Welcome")
+// , ("czech", "Vitejte")
+// , ("danish", "Velkomst")
+// , ("dutch", "Welkom")
+// , ("estonian", "Tere tulemast")
+// , ("finnish", "Tervetuloa")
+// , ("flemish", "Welgekomen")
+// , ("french", "Bienvenue")
+// , ("german", "Willkommen")
+// , ("irish", "Failte")
+// , ("italian", "Benvenuto")
+// , ("latvian", "Gaidits")
+// , ("lithuanian", "Laukiamas")
+// , ("polish", "Witamy")
+// , ("spanish", "Bienvenido")
+// , ("swedish", "Valkommen")
+// , ("welsh", "Croeso")
+// ]
+
+
+
+// Logical Solution
+function greet(lang) {
+	return langs[lang]||langs['english'];
+}
+
+var langs = {
+'english': 'Welcome',
+'czech': 'Vitejte',
+'danish': 'Velkomst',
+'dutch': 'Welkom',
+'estonian': 'Tere tulemast',
+'finnish': 'Tervetuloa',
+'flemish': 'Welgekomen',
+'french': 'Bienvenue',
+'german': 'Willkommen',
+'irish': 'Failte',
+'italian': 'Benvenuto',
+'latvian': 'Gaidits',
+'lithuanian': 'Laukiamas',
+'polish': 'Witamy',
+'spanish': 'Bienvenido',
+'swedish': 'Valkommen',
+'welsh': 'Croeso'
+};
+
+
+
+// Other Solutions
+function greet(language) {
+	var GreetingsDB = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }, 
+  defaultLanguage = 'english';
+  return GreetingsDB[language] || GreetingsDB[defaultLanguage];
+}
+
+
+
+// 2
+var database = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+    
+function greet(language) {
+    return database[language] || "Welcome";
+}
+
+
+// 3
+var languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+function greet(language) {
+    for(var k in languages){
+        if(k == language){
+            return languages[k];
+        }
+    }
+    return "Welcome";
+}
+
+
+
+// 4
+function greet(language) {
+  
+	switch(language){
+    case "english": return "Welcome";
+    case "czech": return "Vitejte";
+    case "danish": return "Velkomst";
+    case "dutch": return "Welkom";
+    case "estonian": return "Tere tulemast";
+    case "finnish": return "Tervetuloa";
+    case "flemish": return "Welgekomen";
+    case "french": return "Bienvenue";
+    case "german": return "Willkommen";
+    case "irish": return "Failte";
+    case "italian": return "Benvenuto";
+    case "latvian": return "Gaidits";
+    case "lithuanian": return "Laukiamas";
+    case "polish": return "Witamy";
+    case "spanish": return "Bienvenido";
+    case "swedish": return "Valkommen";
+    case "welsh": return "Croeso";
+    default: return "Welcome";
+    }
+  
+}
