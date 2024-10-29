18c75fad795db9d1f4fb6b8246d0a50dd7f29bbe
Samantha Lopez <150631576+SamanthaLProgramming@users.noreply.github.com>
2024-10-29 16:41:43 -0700

    Add files via upload

---
+++ LeetCode/Max Num of Characters/main.js
@@ -0,0 +1,31 @@
+// Max Numbers of Characters
+
+// Given a string of characters, return the character that appears the most often.
+
+// No String or Array Methods (well brute force it first, but then no methods)! 
+
+//My Solution
+//string -> 'Hello World!'
+// return the most used chararacter -'l'>
+
+function maxChar(string) {
+	// Create a map to keep track of letters
+	let map = {},
+		count = 0,
+		maxChar = null
+	// Add letters to map
+	for (const char of str) {
+		charMap[char] = charMap[char] + 1 || 1
+	}	
+	// loop & find most freq
+	for (const char in charMap) {
+		if (charMap[char] > count) {
+			count = charMap[char]
+			maxChar = char
+		}
+	}
+
+	return maxChar
+}
+
+console.log(maxChar('Bibbidi Bobbidi Boo!'), 'b')
