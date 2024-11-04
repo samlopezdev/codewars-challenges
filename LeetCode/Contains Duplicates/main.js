6528767a900d802ebaa0e89d6d16ce00a4b0f3b8
Samantha Lopez <150631576+SamanthaLProgramming@users.noreply.github.com>
2024-11-04 13:03:28 -0800

    Add files via upload

---
+++ LeetCode/Contains Duplicates/main.js
@@ -0,0 +1,60 @@
+// Contains Duplicates
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+
+
+
+// My Solution
+const containsDuplicate = function(nums) {
+    const numMap = {}
+
+    for (const n of nums) {
+        if (numMap[n]) {
+            return true
+        } else { numMap[n] = true }
+    }
+
+    return false
+};
+
+
+
+// Other Solutions
+function setSolution(nums) {
+    let testSet = new Set(nums);
+    return testSet.size !== nums.length;
+}
