acb17e58c47056c4dc186ffa1c2fc345644d6e34
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-01-02 23:21:12 -0800

    Update main.js

--- 8Kyu/Multiple of Index/main.js
+++ 8Kyu/Multiple of Index/main.js
@@ -10,7 +10,6 @@
 // [-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
 
 
-
 // My Solution
 function multipleOfIndex(array) {
     return array.filter( (num, i) => num == 0 || num % i == 0)
