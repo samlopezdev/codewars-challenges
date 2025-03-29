52e9acbfc74b41728a2b1fb22524c332d742cf46
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-03-28 19:20:03 -0700

    Update main.js

--- 8Kyu/Making Six Toast/main.js
+++ 8Kyu/Making Six Toast/main.js
@@ -1,7 +1,6 @@
 // Making Six Toast
 
 // You forgot to count the number of toast you put into there, you don't know if you put exactly six pieces of toast into the toasters.
-
 // Define a function that counts how many more (or less) pieces of toast you need in the toasters. Even though you need more or less, the number will still be positive, not negative.
 
 // Examples:
