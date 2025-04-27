b52535ed6f15ceb9317a6ebe951ce55aac5a3431
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-04-26 23:39:44 -0700

    Update main.js

--- LeetCode/Num of Senior Citizen/main.js
+++ LeetCode/Num of Senior Citizen/main.js
@@ -1,7 +1,6 @@
 // Num of Senior Citizens
 
 // You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
-
 // The first ten characters consist of the phone number of passengers.
 // The next character denotes the gender of the person.
 // The following two characters are used to indicate the age of the person.
