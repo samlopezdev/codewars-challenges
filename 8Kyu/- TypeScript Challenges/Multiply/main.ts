72f708903e3c85254506b74baf6bcba549794f9a
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-05-01 14:16:24 -0700

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Multiply/main.ts
@@ -0,0 +1,11 @@
+// Multiply
+
+// The code was not written correctly. Fix it.
+
+// My Solution
+export const multiply = (a: number, b: number): number => a * b;
+
+// Other Solutions
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
