268c4b83e9b3f8ba3ed848a2b592364daff8baea
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-05-04 21:32:15 -0700

    Update main.ts

--- 8Kyu/- TypeScript Challenges/Find the Smallest Int in Array/main.ts
+++ 8Kyu/- TypeScript Challenges/Find the Smallest Int in Array/main.ts
@@ -1,7 +1,6 @@
 // Find the Smallest Int in Array
 
 Given an array of integers your solution should find the smallest integer.
-
 For example:
 Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
