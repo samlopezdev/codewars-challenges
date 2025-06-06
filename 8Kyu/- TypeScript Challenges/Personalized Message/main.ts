17071566cf36bfd52b982b3e24edb564e06701f4
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-06-06 15:26:24 -0700

    Update main.ts

--- 8Kyu/- TypeScript Challenges/Personalized Message/main.ts
+++ 8Kyu/- TypeScript Challenges/Personalized Message/main.ts
@@ -3,8 +3,8 @@
 // Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 // Use conditionals to return the proper message:
 // case	return
-// name equals owner	'Hello boss'
-// otherwise	'Hello guest'
+// name equals owner 'Hello boss'.
+// otherwise	'Hello guest'.
 
 
 // My Solution
