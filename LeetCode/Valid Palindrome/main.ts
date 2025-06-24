6fdc72dd17abeabf256b65f1c9ea8aa39b6b793d
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-06-23 22:35:47 -0700

    Update main.ts

--- LeetCode/Valid Palindrome/main.ts
+++ LeetCode/Valid Palindrome/main.ts
@@ -5,7 +5,6 @@
 // Given a string s, return true if it is a palindrome, or false otherwise.
 
 // Example 1:
-
 // Input: s = "A man, a plan, a canal: Panama"
 // Output: true
 // Explanation: "amanaplanacanalpanama" is a palindrome.
