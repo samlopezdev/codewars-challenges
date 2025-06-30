0681b5f7c759491ac346408b4d5b76701e1cadac
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-06-30 16:12:45 -0700

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Parse Float/main.ts
@@ -0,0 +1,30 @@
+// Parse Float
+
+// Write function parseFloat which takes an input and returns a number or null if conversion is not possible.
+
+// s -> string. Possibly numbers and/or letters.
+// return s if it can be converted to numbers, OR 'Nothing' if it can't
+
+
+// My Solution
+export function parseF(s: string): number | null {
+  return isNaN(parseInt(s)) ? null : Number(s);
+}
+
+// Other Solutions
+export function parseF(s: string): number | null {
+  let float = parseFloat(s);
+
+  return isNaN(float) ? null : float;
+}
+
+// 2
+export function parseF(s: string): number | null {
+  return isNaN(parseFloat(s)) ? null : parseFloat(s);
+}
+
+
+// 3
+export function parseF(s: string): number {
+  return +s ? +s : s === "0" ? 0 : null;
+}
