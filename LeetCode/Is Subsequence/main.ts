4715435bf4086456014d598dbd36cda7f26ad7c5
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-07-20 21:17:18 -0700

    Update main.ts

--- LeetCode/Is Subsequence/main.ts
+++ LeetCode/Is Subsequence/main.ts
@@ -5,7 +5,6 @@
 // A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
  
-
 // Example 1:
 
 // Input: s = "abc", t = "ahbgdc"
