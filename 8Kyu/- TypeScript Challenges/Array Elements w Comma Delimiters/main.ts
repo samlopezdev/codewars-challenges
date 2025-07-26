94e261f8c5c1016386af65556c78539f68f2c778
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-07-25 19:06:55 -0700

    Update main.ts

--- 8Kyu/- TypeScript Challenges/Array Elements w Comma Delimiters/main.ts
+++ 8Kyu/- TypeScript Challenges/Array Elements w Comma Delimiters/main.ts
@@ -1,11 +1,9 @@
 // Array Elements w/ Comma Delimiter
 
 // Input: Array of elements
-
 // ["h","o","l","a"]
 
 // Output: String with comma delimited elements of the array in th same order.
-
 // "h,o,l,a"
 
 // Note: if this seems too simple for you try the next level
