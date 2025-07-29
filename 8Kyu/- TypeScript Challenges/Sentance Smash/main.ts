bde8fc30a7a19196ba95214698faf10880fef4ec
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-07-28 22:28:48 -0700

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Sentance Smash/main.ts
@@ -0,0 +1,42 @@
+// Sentance Smash
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+// Assumptions
+// You can assume that you are only given words.
+// You cannot assume the size of the array.
+// You can assume that you do get an array.
+
+
+// My Solution
+export const smash = (words: string[]): string => words.join(" ");
+
+
+
+// Other Solutions
+export function smash(words: string[]): string {
+  let output: string = "";
+
+  for (let i: number = 0; i < words.length; i++) {
+    if (i != words.length - 1) {
+      output += words[i] + " ";
+    } else if (i == words.length - 1) {
+      output += words[i];
+    }
+  }
+  return output;
+}
+
+
+// 2
+export function smash (words: string[]): string {
+   return words.map((word) => ' ' + word ).join('').trim()
+};
+
+// 3
+export function smash(words: string[]): string {
+  const newStr = words.toString();
+  return newStr.replaceAll(",", " ");
+}
