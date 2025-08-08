c0e45f8b4d9102ddcd77a79e58e00c37742482b2
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-08-08 16:13:56 -0700

    Update main.ts

--- 8Kyu/- TypeScript Challenges/String Templates/main.ts
+++ 8Kyu/- TypeScript Challenges/String Templates/main.ts
@@ -2,7 +2,6 @@
 
 // Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
 
-
 // My Solution
 export function buildString(...template: string[]): string {
   return `I like ${template.join(", ")}!`;
