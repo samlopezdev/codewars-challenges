85918a33410646722fe7e9be7cc28c311de5bed0
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-08-12 23:41:49 -0700

    Update main.ts

--- 8Kyu/- TypeScript Challenges/String Templates/main.ts
+++ 8Kyu/- TypeScript Challenges/String Templates/main.ts
@@ -1,6 +1,6 @@
 // String Templates - Bug Fixing
 
-// Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+// Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects! 
 
 // My Solution
 export function buildString(...template: string[]): string {
