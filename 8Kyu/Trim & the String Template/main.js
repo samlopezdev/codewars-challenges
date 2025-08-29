f189d256d91a44d08f79d565d3b40892a8c1eaef
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-08-29 14:40:18 -0700

    Add files via upload

---
+++ 8Kyu/Trim & the String Template/main.js
@@ -0,0 +1,57 @@
+// Methods of String Object - trim() and the String Template
+
+// Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+// Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+// Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+// Note2: Using a string template can make your job easier.
+
+// Example:
+
+// fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+// a
+// aa
+// aaa
+// aaaa
+// aaaaa       <---The effect when you console.log it
+// fiveLine("  xy ") 
+// should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+// xy
+// xyxy
+// xyxyxy
+// xyxyxyxy
+// xyxyxyxyxy  <---The effect when you console.log it
+
+// My solution
+function fiveLine(s) {
+  s = s.trim();
+  let result = '';
+  for (let i = 1; i <= 5; i++) {
+    result += `${s.repeat(i)}\n`;
+  }
+  return result.trim();
+}
+
+// Other solution
+function fiveLine(s) {
+  s = s.trim();
+  return `${s}\n${s.repeat(2)}\n${s.repeat(3)}\n${s.repeat(4)}\n${s.repeat(5)}`;
+}
+
+
+// 2
+function fiveLine(s) {
+  return [1, 2, 3, 4, 5].map((n) => s.trim().repeat(n)).join("\n");
+}
+
+// 3
+const fiveLine = (s) => {
+  let answer = [];
+  s = s.trim();
+  for (let i = 0; i < 5; i++) {
+    answer.push(s + s.repeat(i));
+  }
+  return answer.join("\n");
+};
