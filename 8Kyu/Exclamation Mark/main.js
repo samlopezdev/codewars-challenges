36c05d256287b0da9c4dc7fd7b636df1e36fbdea
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-09-23 22:38:50 -0700

    Update main.js

--- 8Kyu/Exclamation Mark/main.js
+++ 8Kyu/Exclamation Mark/main.js
@@ -1,6 +1,5 @@
 // Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
 
-
 // Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
 
 // Examples
