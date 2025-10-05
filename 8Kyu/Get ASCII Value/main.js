29d2228b94a02a81163dc7ac61570c73320a564d
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2025-10-04 17:40:54 -0700

    Add files via upload

---
+++ 8Kyu/Get ASCII Value/main.js
@@ -0,0 +1,13 @@
+// Get ASCII Value of Character
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+
+// My Solution
+const getASCII = (c) => c.charCodeAt();
+
+
+// Other Solutions
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
