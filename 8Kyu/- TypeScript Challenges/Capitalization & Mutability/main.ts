679d41ec7994ac825fe0d57ebae0a51dbbc16d91
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2026-01-09 16:12:18 -0800

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Capitalization & Mutability/main.ts
@@ -0,0 +1,21 @@
+// Capitalization & Mutability
+
+// Your coworker was supposed to write a simple helper function to capitalize a string (that contains a single word) before they went on vacation.
+
+// Unfortunately, they have now left and the code they gave you doesn't work. Fix the helper function they wrote so that it works as intended (i.e. it must make the first character in the string upper case).
+
+// The string will always start with a letter and will never be empty.
+
+// Examples:
+
+// "hello" --> "Hello"
+// "Hello" --> "Hello" (the first letter was already capitalized)
+// "a"     --> "A"
+
+
+// My Solution
+const capitalizeWord = (word: string): string => word[0].toUpperCase() + word.slice(1);
+
+// Other Solutions
+const capitalizeWord = (word: string): string =>
+  word.replace(word.charAt(0), word.charAt(0).toUpperCase());
