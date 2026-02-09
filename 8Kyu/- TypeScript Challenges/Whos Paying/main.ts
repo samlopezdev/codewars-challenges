9dcfa40f2e02b37dd70635a3f8140fbe1f2f67d4
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2026-02-09 12:50:05 -0800

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Whos Paying/main.ts
@@ -0,0 +1,29 @@
+// Whos Paying for the Wall
+
+// Don Drumphet lives in a nice neighborhood, but one of his neighbors has started to let his house go. Don Drumphet wants to build a wall between his house and his neighbor’s, and is trying to get the neighborhood association to pay for it. He begins to solicit his neighbors to petition to get the association to build the wall. Unfortunately for Don Drumphet, he cannot read very well, has a very limited attention span, and can only remember two letters from each of his neighbors’ names. As he collects signatures, he insists that his neighbors keep truncating their names until two letters remain, and he can finally read them.
+
+// Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is.
+
+// My Solution
+// first name of person. string
+// return first 2 characters of name. If name is less than or === 2, return name
+export function whoIsPaying(name: string): string[] {
+  return name.length <= 2 ? [name] : [name, name.slice(0, 2)];
+}
+
+// Other Solutions
+export function whoIsPaying(name: string): string[] {
+  if (name.length <= 2) return [name];
+  return [name, name.slice(0, 2)];
+}
+
+// 2
+export function whoIsPaying(name: string): string[] {
+  let arr = [name];
+  if (name.length > 2) arr.push(name.slice(0, 2));
+  return arr;
+}
+
+// 3
+export const whoIsPaying = (name: string): string[] =>
+  name.length > 2 ? [name, name.slice(0, 2)] : [name];
