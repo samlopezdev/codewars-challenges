802938755b7179ed95d9b122ba4711d0f5f48fe3
Samantha Lopez <150631576+samlopezdev@users.noreply.github.com>
2026-02-16 14:43:44 -0800

    Add files via upload

---
+++ 8Kyu/- TypeScript Challenges/Variable Assignment/main.ts
@@ -0,0 +1,22 @@
+// Variable Assignment
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Figure out why and fix it.
+
+// var a == "code";
+// var b == "wa.rs";
+// export var name == a + b;
+
+// My Solution
+let a: string = "code";
+let b: string = "wa.rs";
+export const name: string = a + b;
+
+// Other Solutions
+export const a: string = "code";
+export const b: string = "wa.rs";
+export const name: string = a + b;
+
+// 2
+const a: string = "code";
+const b: string = "wa.rs";
+export const name: string = a + b;
